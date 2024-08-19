0f02cdbe20d965dabf3bfa6bffabfc07b5ae7f40
blfuentes <epiphonetodie@gmail.com>
2024-08-19 19:05:41 +0200

    added hr-sr04 and sharp sensors

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,8 @@
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 
+#include <ultrasonic.h>
+
 #define TAG "main"
 
 extern "C" void app_main();
@@ -89,12 +91,99 @@ void Task1code(void *parameter)
 
 // WIFI
 #define EXAMPLE_ESP_WIFI_SSID "SSID_NAME"
-#define EXAMPLE_ESP_WIFI_PASS "SSID_PASSWORD"
+#define EXAMPLE_ESP_WIFI_PASS "WIFI_PASSWORD"
 
 // IR Sensor
-constexpr adc_channel_t adcChannel = ADC_CHANNEL_6;  // ADC channel for the sensor (GPIO 7)
+constexpr adc_channel_t adcChannel = ADC_CHANNEL_5;  // ADC channel for the sensor (GPIO 7)
 constexpr adc_atten_t adcAtten = ADC_ATTEN_DB_12;    // Attenuation for the ADC
 constexpr adc_bitwidth_t adcWidth = ADC_BITWIDTH_12; // ADC resolution
+
+// HC SR04
+#define TRIGGER_GPIO GPIO_NUM_5
+#define ECHO_GPIO GPIO_NUM_4
+#define MAX_DISTANCE_CM 500 // 5m max
+
+void ultrasonic_test(void *pvParameters)
+{
+    ultrasonic_sensor_t sensor = {
+        .trigger_pin = TRIGGER_GPIO,
+        .echo_pin = ECHO_GPIO
+    };
+
+    ultrasonic_init(&sensor);
+
+    while (true)
+    {
+        float distance;
+        esp_err_t res = ultrasonic_measure(&sensor, MAX_DISTANCE_CM, &distance);
+        if (res != ESP_OK)
+        {
+            printf("Error %d: ", res);
+            switch (res)
+            {
+                case ESP_ERR_ULTRASONIC_PING:
+                    printf("Cannot ping (device is in invalid state)\n");
+                    break;
+                case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
+                    printf("Ping timeout (no device found)\n");
+                    break;
+                case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
+                    printf("Echo timeout (i.e. distance too big)\n");
+                    break;
+                default:
+                    printf("%s\n", esp_err_to_name(res));
+            }
+        }
+        else
+            printf("Distance: %0.04f cm\n", distance*100);
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+// SHARP GP2Y0A51SK0F
+constexpr adc_channel_t adcChannelSharp = ADC_CHANNEL_6;  // ADC channel for the sensor (GPIO 6)
+constexpr float referenceVoltage = 5.0;                 // Reference voltage of the ADC
+constexpr int adcMaxValue = 4096;                       // Maximum ADC value for a 12-bit ADC
+
+float read_adc(adc_oneshot_unit_handle_t handle, adc_channel_t channel)
+{
+    int adcValue = 0;
+    for (uint16_t c = 0; c < 10; c++)
+    {
+        ESP_ERROR_CHECK(adc_oneshot_read(handle, adcChannelSharp, &adcValue));
+        adcValue += adcValue;
+    }
+    return adcValue / 10.0;
+}
+
+float get_voltage(float adcValue)
+{
+    return (adcValue / adcMaxValue) * referenceVoltage;
+}
+
+float get_adc_value(float voltage)
+{
+    return (voltage / referenceVoltage) * adcMaxValue;
+}
+
+float get_distance(float voltage)
+{
+    // The equation of the line is y = a/x + b
+    // and two (x,y) points on the graph:
+    // (30mm, 1.68V) and (150mm, 0.39V)
+    const float a = 48.375;
+    const float b = 0.0675;
+    float dist = 0;
+    if (voltage > b)
+    {
+        dist = a / (voltage - b);
+    }
+    // alternative formula: https://robojax.com/learn/arduino/?vid=robojax_SHARP_0A51SK_IR
+    // float dist = 33.9 + -69.5 * (voltage) + 62.3 * pow(voltage, 2) + -25.4 * pow(voltage, 3) + 3.83 * pow(voltage, 4);
+
+    return dist;
+}
 
 void app_main()
 {
@@ -123,19 +212,26 @@ void app_main()
     configure_led();
     xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 5, &Task1, 1);
 
-    // Initialize ADC
+    // HC SR04
+    xTaskCreate(ultrasonic_test, "ultrasonic_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+
+    // Initialize ADC Config
     adc_oneshot_unit_handle_t adc_handle;
     adc_oneshot_unit_init_cfg_t adc_init_cfg = {
         .unit_id = ADC_UNIT_1,
         .ulp_mode = ADC_ULP_MODE_DISABLE
     };
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&adc_init_cfg, &adc_handle));
-    //-------------ADC1 Config---------------//
     adc_oneshot_chan_cfg_t adc_chan_cfg = {
         .bitwidth = adcWidth,
     };
     adc_chan_cfg.atten = adcAtten;
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, adcChannel, &adc_chan_cfg));
+
+    // Initialize ADC IR Sensor
+    // ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, adcChannel, &adc_chan_cfg));
+
+    // Initialize ADC SHARP Sensor
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, adcChannelSharp, &adc_chan_cfg));
 
     // WIFI
     ESP_LOGI(TAG, "Connecting to AP");
@@ -160,11 +256,20 @@ void app_main()
 
     vTaskDelay(3000 / portTICK_PERIOD_MS);
 
+    // IR Sensor
     int sensorValue = 0;
+
+    // SHARP Sensor
+    float sensorValueSharp = 0.0;
     while (true)
     {
         ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, adcChannel, &sensorValue));
-        ESP_LOGI(TAG, "ADC CH: %d", sensorValue);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "IR value: %d", sensorValue);
+        sensorValueSharp = read_adc(adc_handle, adcChannelSharp);
+        float voltage = get_voltage(sensorValueSharp);
+        float distance = get_distance(voltage);
+        ESP_LOGI(TAG, "SHARP value: %.2f - voltage: %.2f - distance: %.2f", sensorValueSharp, voltage, distance);
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
